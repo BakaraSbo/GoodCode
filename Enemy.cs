75c63c92bff3e86d95c3292fc53f8face9af0c2f
BakaraSbo <48443684+BakaraSbo@users.noreply.github.com>
2022-03-31 07:26:48 +0200

    Enemy classes from my own game

---
+++ Enemy.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed, random;
+    bool alive = true;
+    public GameObject Explosion, AttackSpeed, ScoreUp, ExtraLife;
+    void Update()
+    {
+        transform.position = new Vector2(transform.position.x, transform.position.y - speed*Time.deltaTime);
+        if (transform.position.y < -20)
+        {
+            transform.position = new Vector2(Random.Range(-18, 18), 33);
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ((collision.tag == "PlayerBulletTag") || (collision.tag == "PlayerShipTag") || (collision.tag == "Shield"))
+        {
+            if (alive == true)
+                GameObject.Find("Level").GetComponent<LevelText>().enemiesAlive -= 1;
+            alive = false;
+            GameObject explosion = (GameObject)Instantiate(Explosion);
+            explosion.transform.position = transform.position;
+            random = Random.Range(1, 100);
+            if (random <= 5 && random >= 1)
+            {
+                GameObject attackSpeed = (GameObject)Instantiate(AttackSpeed);
+                attackSpeed.transform.position = transform.position;
+            }
+            if (random <= 10 && random >= 6)
+            {
+                GameObject scoreUp = (GameObject)Instantiate(ScoreUp);
+                scoreUp.transform.position = transform.position;
+            }
+            if (random <= 13 && random >= 11)
+            {
+                GameObject extralife = (GameObject)Instantiate(ExtraLife);
+                extralife.transform.position = transform.position;
+            }
+            if (GameObject.Find("Player").GetComponent<Player>().DoubleScore == true)
+                GameObject.Find("Player").GetComponent<Player>().Score += 40;
+            else
+                GameObject.Find("Player").GetComponent<Player>().Score += 20;
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ EnemyShooting.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooting : MonoBehaviour
+{
+    public float firerate, count = 0, offsetX, offsetY, type;
+    public GameObject Bullet, BulletL, BulletR, Rocket;
+    
+    void Update()
+    {
+        if (count >= firerate)
+        {
+            if (type == 0)
+            {
+                if (count >= firerate)
+                {
+                    GameObject bullet = (GameObject)Instantiate(Bullet);
+                    bullet.transform.position = transform.position;
+                    count %= firerate;
+                }
+            }
+            else if (type == 1)
+            {
+                GameObject bullet = (GameObject)Instantiate(BulletL);
+                bullet.transform.position = transform.position;
+                GameObject bullet2 = (GameObject)Instantiate(BulletR);
+                bullet2.transform.position = transform.position;
+                count %= firerate;
+            }
+            else if (type == 2)
+            {
+                if (count >= firerate)
+                {
+                    GameObject rocket = (GameObject)Instantiate(Rocket);
+                    rocket.transform.position = new Vector2(transform.position.x + offsetX, transform.position.y - offsetY);
+                    GameObject rocket1 = (GameObject)Instantiate(Rocket);
+                    rocket1.transform.position = new Vector2(transform.position.x - offsetX, transform.position.y - offsetY);
+                    count %= firerate;
+                }
+            }
+        }
+        count += Time.deltaTime*5;
+
+    }
+}
